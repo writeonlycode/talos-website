8a456a5b15a773f0b3bbf4de971f1898f0b6de1c
Iago Bozza <iagobozza@writeonlycode.io>
2022-03-15 10:50:54 -0300

    Updates GA

--- v0.6/themes/docsy/postcss.config.js
+++ v0.6/themes/docsy/postcss.config.js
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-module.exports = {
-    plugins: {
-        autoprefixer: {}
-    },
-}
+// module.exports = {
+//     plugins: {
+//         autoprefixer: {}
+//     },
+// }
